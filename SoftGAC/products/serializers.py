8df496e6c5115be4a776e17b3f63fcb86765f0fd
cristianV12 <cristianvelasco0312@gmail.com>
2021-11-07 23:41:16 -0500

    fixes to api

--- SoftGAC/products/serializers.py
+++ SoftGAC/products/serializers.py
@@ -8,6 +8,11 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
   nombre = serializers.CharField(max_length=20,required=True)
   precio = serializers.IntegerField(required=True)
+  descripcion = serializers.CharField(max_length=1000)
+  categoria = serializers.CharField(max_length=20, required=True)
+  dimensiones = serializers.CharField(max_length=20, required=True)
+  referencia = serializers.CharField(max_length=1000)
+  foto = serializers.CharField(max_length=256, required=True)
 
   def create(self, validated_data):
     # Una vez los datos solicitados han sido validados,
@@ -16,15 +21,38 @@ class ProductSerializer(serializers.ModelSerializer):
     return Product.objects.create(
       nombre = validated_data.get('nombre'),
       precio = validated_data.get('precio'),
+      descripcion = validated_data.get('descripcion'),
+      categoria = validated_data.get('categoria'),
+      dimensiones = validated_data.get('dimensiones'),
+      referencia = validated_data.get('referencia'),
+      foto = validated_data.get('foto'),
     )
 
   def update(self, instance, validated_data):
     # Una vez los datos han sido validados,
     # podemos actualizar una instancia del producto en base de datos
+    #TODO --- Refactorizar con un bucle
     instance.nombre = validated_data.get('nombre', instance.nombre)
     instance.save()
-    instance.precio = validated_data.get('precio', instance.nombre)
+    
+    instance.precio = validated_data.get('precio', instance.descripcion)
     instance.save()
+    
+    instance.descripcion = validated_data.get('descripcion', instance.descripcion)
+    instance.save()
+    
+    instance.categoria = validated_data.get('categoria', instance.categoria)
+    instance.save()
+    
+    instance.dimensiones = validated_data.get('dimensiones', instance.dimensiones)
+    instance.save()
+    
+    instance.referencia = validated_data.get('referencia', instance.referencia)
+    instance.save()
+    
+    instance.foto = validated_data.get('foto', instance.foto)
+    instance.save()
+
     return instance
 
   class Meta:
@@ -32,7 +60,8 @@ class ProductSerializer(serializers.ModelSerializer):
     fields = [ 
       'id', 
       'nombre', 
-      'precio', 
+      'precio',
+      'descripcion', 
       'categoria', 
       'dimensiones', 
       'referencia', 
